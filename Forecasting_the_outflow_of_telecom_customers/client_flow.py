61868b6db5c0ee261b8c2c804a7c4274dd36556e
Danil-Belohvostik <danil1994b@yandex.ru>
2022-08-04 13:07:25 +0300

    настройка загрузки данных

--- Forecasting_the_outflow_of_telecom_customers/client_flow.py
+++ Forecasting_the_outflow_of_telecom_customers/client_flow.py
@@ -20,7 +20,7 @@ st.title('Прогноз оттока клиентов')
 # Зададим название файла с параметрами модели
 # joblib_file = 'joblib_cbc.pkl'
 # joblib_file = "joblib_cbc.pkl"
-joblib_file = '/joblib_cbc.pkl'
+joblib_file = 'joblib_cbc.pkl'
 
 
 
@@ -49,8 +49,8 @@ if df is not None:
     test = proc.entire_graph(df, delete_features)
 
     # Загрузим параметры модели с помощью инструмента load библиотеки joblib
-    with open(joblib_file, 'rb') as f:
-        joblib_cbc = joblib.load(f)
+    
+    joblib_cbc = joblib.load(joblib_file)
 
     # Сделаем предсказание
     joblib_cbc_predict = joblib_cbc.predict(test)
