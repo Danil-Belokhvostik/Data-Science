673412a2c84d0390de0655b53b260cac28c00794
Danil-Belohvostik <danil1994b@yandex.ru>
2022-08-04 11:39:42 +0300

    Настройка фонового изображения

--- Forecasting_the_outflow_of_telecom_customers/client_flow.py
+++ Forecasting_the_outflow_of_telecom_customers/client_flow.py
@@ -10,9 +10,9 @@ st.set_page_config(page_icon="🏧", page_title="Client_outflow")
 #image = Image.open('img.png')
 #uploaded_img = st.file_uploader("img",type="jpg")
 #st.open(uploaded_img)
-img = Image.open("https://github.com/Danil-Belokhvostik/Data-Science/blob/main/Forecasting_the_outflow_of_telecom_customers/img.jpg")
-st.image(img, width = 200 )
-#st.image(r'img.png')
+#img = Image.open("https://github.com/Danil-Belokhvostik/Data-Science/blob/main/Forecasting_the_outflow_of_telecom_customers/img.jpg")
+#st.image(img, width = 200 )
+st.image('https://github.com/Danil-Belokhvostik/Data-Science/blob/main/Forecasting_the_outflow_of_telecom_customers/img.jpg')
 
 st.title('Прогноз оттока клиентов')
 
